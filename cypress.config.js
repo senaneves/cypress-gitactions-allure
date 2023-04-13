e2a7b671310d1291ef8b042fc530684dc9ae304b
Fernando Papito <papitorocks@gmail.com>
2023-04-13 20:36:00 -0300

    allure reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  projectId: 'u5x39s',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
+    },
+    env: {
+      allure: true,
+      allureAddVideoOnPass: true
     },
     viewportWidth: 1280,
     viewportHeight: 720,
